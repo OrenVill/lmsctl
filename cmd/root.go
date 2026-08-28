f5b3878839652e0b22ac1b2f80c7278e2b04daf8
Orenvill <oren.villaret@gmail.com>
2026-08-28 13:36:01 +0200

    Scaffold lmsctl Go module and root command

---
+++ cmd/root.go
@@ -0,0 +1,33 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var (
+	flagHost  string
+	flagToken string
+	flagJSON  bool
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "lmsctl",
+	Short: "Manage a remote LM Studio instance from the command line",
+}
+
+// Execute runs the root command and exits the process on error.
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, "Error:", err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	rootCmd.PersistentFlags().StringVar(&flagHost, "host", "", "LM Studio host:port (overrides config file and LMSCTL_HOST)")
+	rootCmd.PersistentFlags().StringVar(&flagToken, "token", "", "LM Studio API token (overrides config file and LMSCTL_TOKEN)")
+	rootCmd.PersistentFlags().BoolVar(&flagJSON, "json", false, "output machine-readable JSON")
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "lmsctl/cmd"
+
+func main() {
+	cmd.Execute()
+}
